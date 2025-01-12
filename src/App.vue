a841b2d308e4bd6f128a9cd6fb3a306e78b48edc
hannan84 <hannansarkar84@gmail.com>
2025-01-12 18:42:00 +0600

    binding-data with html attribute

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,71 @@
 <script setup>
-import data from "./data/data.js";
+const images = [
+  {
+    image:
+      "//images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    title: "Surrounded by trees and mountains",
+    link: "https://unsplash.com/photos/NRQV-hBF10M",
+  },
+  {
+    image:
+      "//images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    title: "Water surrounded by trees and mountains",
+    link: "https://unsplash.com/photos/NRQV-hBF10M",
+  },
+  {
+    image:
+      "//images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    title: "Mountains and Trees",
+    link: "https://unsplash.com/photos/NRQV-hBF10M",
+  },
+];
 </script>
 
 <template>
-  <div
-    class="container mx-auto flex items-center justify-center min-h-screen flex-col"
-  >
-    <h1 class="text-4xl">{{ data.message }}</h1>
-    <h2 class="mt-10 text-xl text-gray-700">
-      Today is <span>{{ data.date }}</span>
-    </h2>
-  </div>
+  <section class="container mx-auto p-10">
+    <h1 class="text-4xl mb-10">Data Project</h1>
+    <section class="main flex space-x-4">
+      <div class="flex space-y-4 flex-col w-1/2">
+        <img :title="images[0].title" v-bind:src="images[0].image" alt="" />
+        <h2 class="text-xl text-left">
+          {{ images[0].title }}
+        </h2>
+        <a
+          target="_blank"
+          :href="images[0].link"
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded w-40 text-center"
+        >
+          Image Source
+        </a>
+      </div>
+      <div class="flex space-y-4 flex-col w-1/2">
+        <img :title="images[1].title" :src="images[1].image" alt="" />
+        <h2 class="text-xl text-left">
+          {{ images[1].title }}
+        </h2>
+        <a
+          target="_blank"
+          :href="images[1].link"
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded w-40 text-center"
+        >
+          Image Source
+        </a>
+      </div>
+      <div class="flex space-y-4 flex-col w-1/2">
+        <img :title="images[2].title" v-bind:src="images[2].image" alt="" />
+        <h2 class="text-xl text-left">
+          {{ images[2].title }}
+        </h2>
+        <a
+          target="_blank"
+          :href="images[2].link"
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded w-40 text-center"
+        >
+          Image Source
+        </a>
+      </div>
+    </section>
+  </section>
 </template>
 
 <style scoped></style>
