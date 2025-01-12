9845375e83a3d7656d3d0fc9976336206e74a847
hannan84 <hannansarkar84@gmail.com>
2025-01-12 15:33:55 +0600

    using static data to veriable data

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
-<script setup></script>
+<script setup>
+const message = "Welcome. Get ready to master Vue.js 3!";
+// const date = "30th June, 2025";
+function getDate() {
+  const date = new Date();
+  return date.toDateString();
+}
+</script>
 
 <template>
   <div
     class="container mx-auto flex items-center justify-center min-h-screen flex-col"
   >
-    <h1 class="text-4xl">Welcome. Get ready to master Vue.js 3!</h1>
+    <h1 class="text-4xl">{{ message }}</h1>
     <h2 class="mt-10 text-xl text-gray-700">
-      Today is <span>30th June, 2025</span>
+      Today is <span>{{ getDate() }}</span>
     </h2>
   </div>
 </template>
