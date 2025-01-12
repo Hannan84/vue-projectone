553956c3992febf3783e87f61ed87e10fa00b6be
hannan84 <hannansarkar84@gmail.com>
2025-01-12 15:46:15 +0600

    display data from object-array

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <script setup>
 const message = "Welcome. Get ready to master Vue.js 3!";
-// const date = "30th June, 2025";
+const date = "30th June, 2025";
 function getDate() {
   const date = new Date();
   return date.toDateString();
 }
+
+const data = {
+  message: "Welcome. Get ready to master Vue.js 3!",
+  date: "30th June, 2025",
+};
 </script>
 
 <template>
   <div
     class="container mx-auto flex items-center justify-center min-h-screen flex-col"
   >
-    <h1 class="text-4xl">{{ message }}</h1>
+    <h1 class="text-4xl">{{ data.message }}</h1>
     <h2 class="mt-10 text-xl text-gray-700">
-      Today is <span>{{ getDate() }}</span>
+      Today is <span>{{ data.date }}</span>
     </h2>
   </div>
 </template>
